13f1ab92a123ab495019506ddd07174ac7e762a5
David Eduardo Peña Tinco <u23227035@utp.edu.pe>
2025-09-20 08:18:54 -0500

    Add files via upload

---
+++ Auto_TEC/Auto_TEC/Auto_TEC/Auto_TEC/src/main/java/com/example/Auto_TEC/AutoTecApplication.java
@@ -0,0 +1,13 @@
+package com.example.Auto_TEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTecApplication.class, args);
+	}
+
+}
