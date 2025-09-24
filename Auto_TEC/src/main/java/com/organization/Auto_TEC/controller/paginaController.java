09d5f9c2c18a956c6d50cc59cc92eed4f78cb3bd
Josue Rodrigo Rojas Taboada <u23249497@utp.edu.pe>
2025-09-24 18:45:10 -0500

    actualización de tablas y propiedades

--- Auto_TEC/src/main/java/com/organization/Auto_TEC/controller/paginaController.java
+++ Auto_TEC/src/main/java/com/organization/Auto_TEC/controller/paginaController.java
@@ -1,4 +1,4 @@
-package com.organization.Auto_TEC.Controller;
+package com.organization.Auto_TEC.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
